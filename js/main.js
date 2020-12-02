19886845df66c7c72373fc251ea92a5b233c8b36
Flavio Muscente <flaviomuscente@libero.it>
2020-12-02 23:46:53 +0100

    add vue search movie

--- js/main.js
+++ js/main.js
@@ -3,14 +3,35 @@ Vue.config.devtools = true;
 var app = new Vue({
   el: '#app',
   data: {
-
+    movies:[],
+    inputQuery: "",
+    imgsrc: "https://image.tmdb.org/t/p/w500"
   },
-  mounted(){
-    //prelevo i dati tramite axios
-    axios.get('').then(()=> {
-    });
+  methods:{
+    getMovie(){
 
-  },
-  computed:{
+      const API_KEY = 'b6953a45324a16a37c8d476e528ee699';
+  
+      axios.get('https://api.themoviedb.org/3/search/movie/', {
+        params:{
+          'api_key' : API_KEY,
+          query : this.inputQuery
+        }
+      }).then((mymovie)=> {
+           this.movies = mymovie.data.results;
+      });
+    },
+    imgSrc(mov){
+      let mysource = this.imgsrc + mov.poster_path;
+      if(mov.poster_path == null){
+         mysource = "img/not-found.png";
+      }
+      return mysource;
+    },
+    flagLanguage(mov){
+      let langsrc = "img/" + mov.original_language + ".svg";
+      return langsrc;
+    }
+
   }
 });
