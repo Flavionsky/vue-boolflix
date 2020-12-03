1e08b6bdfe5e0864bf42a831f4b593cba7439d85
Flavio Muscente <flaviomuscente@libero.it>
2020-12-03 18:21:21 +0100

    add star , botton next and prev page and film concat with movie

--- js/main.js
+++ js/main.js
@@ -4,22 +4,36 @@ var app = new Vue({
   el: '#app',
   data: {
     movies:[],
+    display: null,
     inputQuery: "",
+    nextPage: 1,
+    isActive: false,
     imgsrc: "https://image.tmdb.org/t/p/w500"
   },
   methods:{
     getMovie(){
 
       const API_KEY = 'b6953a45324a16a37c8d476e528ee699';
-  
-      axios.get('https://api.themoviedb.org/3/search/movie/', {
-        params:{
-          'api_key' : API_KEY,
-          query : this.inputQuery
-        }
-      }).then((mymovie)=> {
-           this.movies = mymovie.data.results;
-      });
+
+        axios.get('https://api.themoviedb.org/3/search/movie/', {
+          params:{
+            'api_key' : API_KEY,
+            query : this.inputQuery,
+            page : this.nextPage
+          }
+        }).then((mymovie)=> {
+             this.movies = mymovie.data.results;
+        });
+        axios.get('https://api.themoviedb.org/3/search/tv/', {
+            params:{
+              'api_key' : API_KEY,
+              query : this.inputQuery,
+              page : this.nextPage
+            }
+          }).then((tv)=> {
+              this.movies = this.movies.concat(tv.data.results);
+          });
+          this.isActive = true;
     },
     imgSrc(mov){
       let mysource = this.imgsrc + mov.poster_path;
@@ -28,10 +42,23 @@ var app = new Vue({
       }
       return mysource;
     },
-    flagLanguage(mov){
-      let langsrc = "img/" + mov.original_language + ".svg";
-      return langsrc;
+    myNext(){
+      this.nextPage++;
+      this.getMovie();
+    },
+    myBack(){
+      this.nextPage--;
+      if(this.nextPage == 0){
+        this.nextPage = 1;
+      }
+      this.getMovie();
+    },
+    changeTitle(mov){
+      let title = mov.title;
+      if(!mov.title){
+        title = mov.name;
+      }
+      return title;
     }
-
   }
 });
